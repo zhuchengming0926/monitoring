b51f2da5ae706e97d4f307416c250c9be2d72ce6
zcm <1207030053@qq.com>
2017-09-12 18:26:22 +0800

    Update mojiweather_monitoring.sh

--- mojiweather_monitoring.sh
+++ mojiweather_monitoring.sh
@@ -1,8 +1,8 @@
 #!/bin/env bash
 now=`date +%s`
-last_innerupdatedtime=`mysql -hros-user.mysql.rds.aliyuncs.com -uros_service -pyBEjCPiw7lQPlDEGndB2 -e "select * from mojiweather.innercity_weather_history order by updated_at desc limit 1\G;"|grep updated_at|awk '{print $2}'`
+last_innerupdatedtime=`mysql -hxxxx -uros_service -xxx -e "select * from mojiweather.innercity_weather_history order by updated_at desc limit 1\G;"|grep updated_at|awk '{print $2}'`
 
-last_outerupdatedtime=`mysql -hros-user.mysql.rds.aliyuncs.com -uros_service -pyBEjCPiw7lQPlDEGndB2 -e "select * from mojiweather.outercity_weather_history order by updated_at desc limit 1\G;"|grep updated_at|awk '{print $2}'`
+last_outerupdatedtime=`mysql -hxxxx -uros_service -xxxx -e "select * from mojiweather.outercity_weather_history order by updated_at desc limit 1\G;"|grep updated_at|awk '{print $2}'`
 if [ $(($now-$last_innerupdatedtime)) -gt $((9*3600)) ] || [ $(($now-$last_outerupdatedtime)) -gt $((9*3600)) ];then
   echo 0 > ./mojiweather_status
   exit 1;
